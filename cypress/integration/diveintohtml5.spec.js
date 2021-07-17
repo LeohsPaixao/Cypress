7c945221992aa52e9df4320012b0269f86eb4fd4
Leonardo Paixão <leonardohspaixao@gmail.com>
2021-07-17 20:01:22 -0300

    Creation more test in Dive Into HTML5

--- cypress/integration/diveintohtml5.spec.js
+++ cypress/integration/diveintohtml5.spec.js
@@ -10,6 +10,39 @@ describe("Dive Into HTML5", () => {
     })
 
     it("check text", () => {
-        cy.get("p abbr").should("contain", "HTML5")
+        cy.get("p.f a").should("contain", "HTML5");
     })
+
+    it("is there any summary around here", () => {
+        cy.get("h3").should("contain", "Sumário")
+    })
+
+    it("check the name the first link", () => {
+        cy.get("ol li span.title").should("contain", "Introdução: Cinco Coisas Que Você Deveria Saber Sobre");
+    })
+
+    /*
+    it("click on second link", () => {
+        cy.get("ol li span.title").click({multiple: true}) //SecurityError
+    })
+    */
+
+    it("check paragraphs", () => {
+        cy.get("p.c span.u").should("contain", "—")
+    })
+
+    it("click on link Mark Pilgrim", () => {
+        cy.get("p.c a").click({multiple: true})
+    })
+
+    it("fills one the text input fields", () => {
+        cy.get("form div input[name=q]").type("HMTL5");
+    })
+
+    /*
+    it("click on Search", () => {
+        cy.get("form div input[name=sa]").click() //Permition Denied
+    })
+    */
+
 })
